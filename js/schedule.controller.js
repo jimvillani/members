3152b132ba8473aac9e6678255c9735bcd857e67
Jim Villani <jimvillani@icloud.com>
2018-11-06 20:16:06 -0500

    2018-19 update

--- js/schedule.controller.js
+++ js/schedule.controller.js
@@ -3,7 +3,8 @@ app.controller('scheduleCtrl', function ($scope, $http) {
     $http.get("data/schedule.txt")
         .then(function (response) {
             $scope.scheduleData = response.data.schedule;
-            $scope.colLength = (response.data.schedule.length + (3 - response.data.schedule.length % 3))/3;
-            // $scope.colLength = response.data.schedule.length / 3;
+            $scope.schedLength = $scope.scheduleData.length;
+            //if not divisible by 3, add 1 to the column
+            $scope.colLength = Math.floor((response.data.schedule.length / 3) + (response.data.schedule.length % 3 > 0));
         });
 });
